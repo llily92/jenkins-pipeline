7358c3fa205e24210ac6e32ab2517a5a45fd6148
Lily K <kuzyshynliliia@gmail.com>
2020-04-01 17:15:30 -0500

    added some comments

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -1,5 +1,10 @@
 node { 
-properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), pipelineTriggers([cron('* * * * *')])])
+properties([
+    // Below line sets "Discard build more than 5"
+    buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')),
+    // Below line triggers this job every minute 
+     pipelineTriggers([cron('* * * * *')])
+     ])
 
 
 
