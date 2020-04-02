618090b4c23959107192a0293c4776c2a2684b7e
Lily K <kuzyshynliliia@gmail.com>
2020-04-01 19:21:26 -0500

    added env

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -3,7 +3,7 @@ properties([
     // Below line sets "Discard build more than 5"
     buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')),
     // Below line triggers this job every minute 
-     pipelineTriggers([pollSCM('* * * * *')])
+     pipelineTriggers([pollSCM('* * * * *')]),
      parameters([choice(choices: [
 			'dev1.kuzyshynliliia.com', 
 			'qa1.kuzyshynliliia.com', 
