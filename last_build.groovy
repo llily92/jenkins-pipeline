12090dc142d05f63d586f036130cbf5c0b091a73
Lily K <kuzyshynliliia@gmail.com>
2020-04-02 19:51:05 -0500

    added last build

---
+++ last_build.groovy
@@ -0,0 +1,30 @@
+node { 
+    // Keeps only 5 builds
+properties([
+    buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '5', numToKeepStr: '')),
+   pipelineTriggers([
+       // Runs with "Input" job
+        upstream('Input'),
+        // Executes every minute
+        cron('* * * * *')])
+        ])
+    ])
+
+stage("Stage1"){ 
+git 'https://github.com/farrukh90/packer.git' 
+} 
+stage("Stage2"){ 
+echo "hello" 
+} 
+stage("Stage3"){ 
+echo "hello" 
+} 
+stage("Stage4"){ 
+echo "hello" 
+} 
+stage("Stage5"){ 
+node { 
+// some block 
+}  
+} 
+} 
